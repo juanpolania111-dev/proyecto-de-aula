8199cfc4987e1bb313a9e2bc08705822736c0fca
jurgenlucky <jurgen1018@gmail.com>
2026-08-21 17:09:34 -0500

    Ejercicio 1: enunciado literal completo (faltaban frases del original)

--- ejercicios/ejercicio 1.c
+++ ejercicios/ejercicio 1.c
@@ -1,10 +1,11 @@
 /*
-Ejercicio 1: Una variable llamada NUMERO contiene un entero de 4 digitos (mayor o igual a
-1000 y menor o igual 9999). En caso que la variable NUMERO este fuera del rango entonces no
-se debe ejecutar el programa. Mostrar en pantalla cual es la unidad de mil, la centena, la
-decena y la unidad. No usar arreglos, vectores, listas o cualquier otro dato de ese tipo, la
-variable NUMERO debe ser de tipo entero. Resultado esperado: {'Numero' : NUMERO,
-'Unidad' : UNIDAD, 'Decena' : DECENA, 'Centena' : CENTENA, 'Unidad de mil' : UNIDAD_DE_MIL}
+Ejercicio 1: Una variable llamada NUMERO contiene un entero de 4 dígitos (mayor o igual a
+1000 y menor o igual 9999). En caso que la variable NUMERO esté fuera del rango (menor a
+1000 o mayor a 9999) entonces no se debe ejecutar el programa. Mostrar en pantalla cual es
+la unidad de mil, la centena, la decena y la unidad. No usar arreglos, vectores, listas o
+cualquier otro dato de ese tipo, la variable NUMERO debe ser de tipo entero. Resultado
+esperado en pantalla: {'Número' : NUMERO, 'Unidad' : UNIDAD, 'Decena' : DECENA, 'Centena' :
+CENTENA, 'Unidad de mil' : UNIDAD_DE_MIL}
 
 # Proyecto de Aula
 
