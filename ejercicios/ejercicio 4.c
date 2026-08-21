9bfb7dcbc6ab76ed62df0c579fad3b600e7c6f35
jurgenlucky <jurgen1018@gmail.com>
2026-08-21 17:09:46 -0500

    Ejercicio 4: corregir numero interno (decia Ejercicio 1) y enunciado literal

--- ejercicios/ejercicio 4.c
+++ ejercicios/ejercicio 4.c
@@ -1,13 +1,11 @@
-
 /*
-Ejercicio 1:
-Se puede calcular el cuadrado de un número NUMERO al sumar los primeros NUMERO
+Ejercicio 4: Se puede calcular el cuadrado de un número NUMERO al sumar los primeros NUMERO
 números impares. Con esta lógica, el cuadrado del número 7 es la sumatoria de los primeros
-7 números impares, es decir 1+3+5+7+9+11+13=49. Crear un programa que pida al usuario un número
-y se almacene en la variable NUMERO y luego imprima la suma de todos los números impares y
-su resultado.
-*/
-/*# Proyecto de Aula
+7 números impares, es decir 1+3+5+7+9+11+13=49. Crear un programa que pida al usuario un
+número y se almacene en la variable NUMERO y luego imprima la suma de todos los números
+impares y su resultado.
+
+# Proyecto de Aula
 
 ## Integrantes
 
